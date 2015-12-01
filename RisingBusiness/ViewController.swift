a0abc88d9ee02c08cb23a29caafa765175991af5
Jack Gregory <jhgvgregory@gmail.com>
2015-11-30 20:19:22 -0500

    fix keyboard on login page to close when the "done" button is pressed

--- RisingBusiness/ViewController.swift
+++ RisingBusiness/ViewController.swift
@@ -8,17 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.emailField.delegate = self
+        self.passwordField.delegate = self
+        self.emailField.returnKeyType = UIReturnKeyType.Done
+        self.passwordField.returnKeyType = UIReturnKeyType.Done
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    
 
 
 }
