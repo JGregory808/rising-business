4e97859906a4e59ea91aa3958d762790829acb82
alex-oconnor <aoconno8@bu.edu>
2016-04-11 20:30:12 -0400

    Added Titles to each page, disabled user edit on About Us

--- RisingBusiness/AboutUs.swift
+++ RisingBusiness/AboutUs.swift
@@ -28,3 +28,5 @@ class AboutUs: UIViewController, UITextFieldDelegate {
     
     
 }
+
+ 
---
+++ RisingBusiness/EmailReference.swift
@@ -0,0 +1,11 @@
+//
+//  EmailReference.swift
+//  RisingBusiness
+//
+//  Created by Alex O'Connor on 4/11/16.
+//  Copyright © 2016 GAI. All rights reserved.
+//
+
+import Foundation
+
+@interface
